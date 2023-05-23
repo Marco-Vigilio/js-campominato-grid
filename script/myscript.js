c690afb94bb586db87d0fa0284de7d5aadd51d0c
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-23 15:54:16 +0200

    inserimento del file script

---
+++ script/myscript.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * L'utente clicca su un bottone che genererà una griglia di gioco quadrata. 
+ * Ogni cella ha un numero progressivo, da 1 a 100. 
+ * Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
+ * Quando l'utente clicca su ogni cella, 
+ * la cella cliccata si colora di azzurro ed emetto un messaggio in console 
+ * con il numero della cella cliccata.
+ * 
+ */
+
+console.log("Soono qui");
+
+const bottone = document.querySelector("#play");
+
+bottone.addEventListener("click", function(){
+    console.log("Sono qui");
+});
